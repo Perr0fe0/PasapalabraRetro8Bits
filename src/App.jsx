37a9ede3a2bcb17c0df03ac822de37e3023839b9
Perr0fe0 <83041787+Perr0fe0@users.noreply.github.com>
2024-06-25 09:33:42 -0400

    Diseño PC y  Telefono parcialmente completado

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+const App = () => {
+  
+  return (
+    <>
+
+      <main className="flex justify-center">
+        <div className="container text-center flex flex-col gap-9">
+          <h1 className="text-5xl mt-10">PasaPalabras <br /> <span>RETRO</span> </h1>
+          <h2>30 Segundos</h2>
+       
+          <div className="flex justify-center gap-2">
+            <div className="border-solid border border-white w-20  h-20 text-7xl uppercase">p</div>
+            <div className="border-solid border border-white w-20  h-20 text-7xl uppercase">e</div>
+            <div className="border-solid border border-white w-20  h-20 text-7xl uppercase">r</div>
+            <div className="border-solid border border-white w-20  h-20 text-7xl uppercase">a</div>
+          </div>
+
+          <div className="grid grid-cols-3 md:grid-cols-9 gap-2 p-2">
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">a</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">b</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">c</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">d</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">e</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">f</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">g</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">h</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">i</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">j</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">k</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">l</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">m</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">n</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">ñ</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">o</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">p</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">q</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">r</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">s</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">t</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">u</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">v</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">w</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">x</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">y</div>
+            <div className="border border-solid border-white md:w-32 md:h-32 md:text-[90px] text-center uppercase hover:text-black hover:bg-white hover:cursor-pointer">z</div>
+            
+              
+            
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
